83e61becb700e52e05238827efad69e20023a8a9
Gustavo Maciel Ramos de Almeida - 824148698 <824148698@ulife.com.br>
2025-03-11 10:28:20 -0300

    AULA11.03

---
+++ variaveis.js
@@ -0,0 +1,20 @@
+var idade = 18;
+let nome = "Gustavo";
+const pi = 3.14;
+let ativo = true;
+
+//conversão de nomero para string
+
+let str = String(100)
+let outrasrt = (42).toString()
+
+
+console.log(idade);
+console.log(nome);
+console.log(pi);
+console.log(ativo);
+
+
+if(idade >= 18) {
+    console.log("maior de idade");
+}
